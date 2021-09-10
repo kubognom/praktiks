b4bfef0d0502f5558d87f0cfe47bd59f9195a845
nikitaverygood <nikitaverygood@gmail.com>
2021-09-10 10:54:11 +0300

    практика1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace PR1_1
 {
+    struct Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Person(string _firstName, string _lastName, int _age)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            age = _age;
+            
+        }
+        
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName + ", age " + age;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Person p = new Person("цыганков", "никита", 19);
+            Console.WriteLine(p);
+            Console.ReadKey();
         }
     }
 }
